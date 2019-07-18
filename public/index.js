8922fce434a039803eb7057401bdf5788f5e6f1a
afeiship <1290657123@qq.com>
2019-07-18 11:37:43 +0800

    add: sub drawer

--- public/index.js
+++ public/index.js
@@ -6,24 +6,40 @@ import './assets/style.scss';
 
 class App extends React.Component {
   state = {
-    value: false
+    value: false,
+    value2: false
   };
 
   _onChange = (e) => {
     const { value } = e.target;
-    console.log('value:->', value);
+    this.setState({ value });
+  };
+
+  _onChange2 = (e) => {
+    const { value } = e.target;
+    this.setState({ value2: value });
   };
 
   _onClick = (e) => {
     this.setState({ value: true });
   };
 
+  _onClick2 = (e) => {
+    this.setState({ value2: true });
+  };
+
   render() {
-    const { value } = this.state;
+    const { value, value2 } = this.state;
+    console.log('value,value2', value, value2);
     return (
       <div className="app-container">
         <Button onClick={this._onClick}>ShowDrawer</Button>
-        <ReactAntDrawer onChange={this._onChange} value={value} />
+        <ReactAntDrawer onChange={this._onChange} value={value}>
+          <ReactAntDrawer
+            onChange={this._onChange2}
+            value={value2}></ReactAntDrawer>
+          <Button onClick={this._onClick2}>SHow Drawer!!!</Button>
+        </ReactAntDrawer>
       </div>
     );
   }
